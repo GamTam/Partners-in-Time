533646a29acdab36a4905186646533cecc9ce0c6
Lucca DaCambra <luccadacambra@gmail.com>
2022-05-01 14:43:24 -0400

    Character Billboarding

--- Assets/Scripts/SpriteChar.cs
+++ Assets/Scripts/SpriteChar.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class SpriteChar : MonoBehaviour
 {
+    private Camera cam;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Quaternion rot = transform.rotation;
         
+        transform.rotation = cam.transform.rotation;
+        
+        transform.rotation = Quaternion.Euler(rot.eulerAngles.x, transform.eulerAngles.y, rot.eulerAngles.z);
     }
 }
