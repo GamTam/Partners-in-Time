1e2d3a9e5980e50ddb538511a9d7692008efd20d
GamTam <luccadacambra@gmail.com>
2022-06-20 18:32:15 -0400

    Non-Interactive Camera

---
+++ Assets/Scripts/AreaTriggers.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaTriggers : MonoBehaviour
+{
+    public GameObject virtualCam;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !(other.isTrigger))
+        {
+            virtualCam.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && !(other.isTrigger))
+        {
+            virtualCam.SetActive(false);
+        }
+    }
+}
