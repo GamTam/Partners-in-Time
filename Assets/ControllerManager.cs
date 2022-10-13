1539e387938c7ea1f7f479ef1f536213ae53f346
Mabel DaCambra <luccadacambra@gmail.com>
2022-10-13 18:19:59 -0400

    Button prompts work regardless of language!

--- Assets/ControllerManager.cs
+++ Assets/ControllerManager.cs
@@ -14,6 +14,9 @@ public class ControllerManager : MonoBehaviour
     [SerializeField] private VariablesGroupAsset _source;
     private PlayerInput _playerInput;
     private string _currentPlayerInput;
+
+    private int _currentControlInt = 0;
+    private string _currentControlerName = "Keyboard";
     
     // Start is called before the first frame update
     void Start()
@@ -53,10 +56,29 @@ public class ControllerManager : MonoBehaviour
         var jumpAction = _source["jump"] as StringVariable;
 
         activeController.Value = _playerInput.currentControlScheme;
-        confirmButton.Value = _playerInput.actions["confirm"].GetBindingDisplayString();
+
+        switch (_playerInput.currentControlScheme)
+        {
+            case "Keyboard":
+                _currentControlInt = 0;
+                _currentControlerName = "Keyboard";
+                break;
+            case "PS4 Controller":
+                _currentControlInt = 1;
+                _currentControlerName = "DualShockGamepad";
+                break;
+            case "Xbox Controller":
+                _currentControlInt = 2;
+                _currentControlerName = "XInputController";
+                break;
+        }
+        
+        Debug.Log($"{_playerInput.actions["confirm"].GetBindingDisplayString()}, {_playerInput.actions["confirm"].bindings[_currentControlInt].ToString().Replace($"confirm:<{_currentControlerName}>/", "").Replace($"[{_playerInput.currentControlScheme}]", "")}");
+
+        confirmButton.Value = _playerInput.actions["confirm"].bindings[_currentControlInt].ToString().Replace($"confirm:<{_currentControlerName}>/", "").Replace($"[{_playerInput.currentControlScheme}]", "");
         switchAction.Value = _playerInput.actions["switch_action"].GetBindingDisplayString();
         mAction.Value = _playerInput.actions["m_action"].GetBindingDisplayString();
         lAction.Value = _playerInput.actions["l_action"].GetBindingDisplayString();
-        jumpAction.Value = _playerInput.actions["jump"].GetBindingDisplayString();
+        jumpAction.Value = _playerInput.actions["jump"].bindings[_currentControlInt].ToString().Replace($"jump:<{_currentControlerName}>/", "").Replace($"[{_playerInput.currentControlScheme}]", "");
     }
 }
