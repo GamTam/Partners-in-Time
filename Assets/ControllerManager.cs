c95e400cb5fdab8bcc280f75705e284031a0be80
Mabel <luccadacambra@gmail.com>
2023-02-22 19:56:58 -0500

    Removed FMOD

--- Assets/ControllerManager.cs
+++ Assets/ControllerManager.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using FMOD.Studio;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Localization.Settings;
--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    private static FMOD.Studio.Bus _masterBus;
     private static GameObject _me;
     
     // Start is called before the first frame update
@@ -17,12 +16,5 @@ public class AudioManager : MonoBehaviour
         }
         
         _me = gameObject;
-        _masterBus = FMODUnity.RuntimeManager.GetBus("Bus:/");
-    }
-
-    // Update is called once per frame
-    public static void FadeoutAll()
-    {
-        _masterBus.stopAllEvents(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
     }
 }
